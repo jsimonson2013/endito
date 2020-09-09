cfeb5171bf8c8d17644ef06b8e6e53e9dc54a0e2
jsimonson2013 <jsimonson2013@gmail.com>
2020-09-09 07:38:56 -0700

    remove active classes

--- interface/index.js
+++ interface/index.js
@@ -4,20 +4,37 @@ window.onload = () => {
             editText(e.target)
         })
     }
+
+    document.addEventListener("click", () => {
+        clearActive()
+    })
+}
+
+function clearActive() {
+    for(let i of document.querySelectorAll(".active-edit-text")) {
+        if (i.innerHTML[i.innerHTML.length - 1] == "|") {
+            i.innerHTML = i.innerHTML.substring(0, i.innerHTML.length - 1)
+        }
+        i.removeEventListener("keyup", textKeyHandler, false)
+        i.classList.remove("active-edit-text")
+        Window.active = null
+    }
 }
 
 function editText(tgt) {
     tgt.classList.add("active-edit-text")
     tgt.innerHTML += "|"
-    document.addEventListener("keyup", (e) => {
-        console.log(e.key)
-        if (e.code == "Backspace"){
-            tgt.innerHTML = tgt.innerHTML.substring(0, tgt.innerHTML.length - 2) + "|"
-        } else if ((e.keyCode <= 90 && e.keyCode >= 48) || 
-            (e.keyCode <= 111 && e.keyCode >= 96) ||
-            (e.keyCode <= 222 && e.keyCode >= 186) || 
-            e.key == " ") {
-            tgt.innerHTML = tgt.innerHTML.substring(0, tgt.innerHTML.length - 1) + e.key + "|"
-        }
-      })
+    Window.active = tgt
+    document.addEventListener("keyup", textKeyHandler, false)      
+}
+
+function textKeyHandler(e) {
+    if (e.code == "Backspace"){
+        Window.active.innerHTML = Window.active.innerHTML.substring(0,Window.active.innerHTML.length - 2) + "|"
+    } else if ((e.keyCode <= 90 && e.keyCode >= 48) || 
+        (e.keyCode <= 111 && e.keyCode >= 96) ||
+        (e.keyCode <= 222 && e.keyCode >= 186) || 
+        e.key == " ") {
+        Window.active.innerHTML = Window.active.innerHTML.substring(0,Window.active.innerHTML.length - 1) + e.key + "|"
+    }
 }
